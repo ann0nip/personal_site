e50a625e5ad2547fa4403eda02ab44e7f9b3bf82
Juan Gimenez <j.martin.utn@gmail.com>
2020-07-04 16:59:30 -0300

    Structure created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Header from "../components/Header/Header"
+import Main from "../components/Main/Main"
+import Details from "../components/Details/Details"
+import Posts from "../components/Posts/Posts"
+import Footer from "../components/Footer/Footer"
 
 export default function Home() {
-  return <h1>JUAN MARTIN GIMENEZ YUNIS!</h1>
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+      <Details />
+      <Posts />
+      <Footer />
+    </React.Fragment>
+  )
 }
